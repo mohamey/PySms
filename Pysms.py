6064ab6e9e4102506780f460a9c2003fc4a9a7b8
mohamey <mohamey@tcd.ie>
2016-04-02 17:22:18 +0100

    Manual python2 to python3 conversion, improved performance
    Converted main script and dependencies to python version 3, made
    performance optimisations, reduced library overhead

--- Pysms.py
+++ Pysms.py
@@ -1,155 +1,133 @@
 import vobject
-from Tkinter import *
+from tkinter import *
+from tkinter import messagebox
 import keyring
-from keyring import *
-import cPickle
-import tkMessageBox
-import cookielib
-import urllib
-import urllib2
-import atexit
 import dependencies.three as three
+import pickle
+import os
+import sys
 
-####initialise url lib dependencies
-cookieJar = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
-opener.addHeaders = [('User-agent', 'Webtext-Tool')]
-
-urllib2.install_opener(opener)
-
-log = open("log.txt", 'wb')
-
-def closeFile(name):
-	name.close()
-
-atexit.register(closeFile, log)
-
+##Destroy the frames in a grid
 def destroyGrid(frame):
 	for widget in frame.grid_slaves():
 		widget.destroy()
 
-####Generate the Login Frame######
+##Save login Details
+def saveLogin(login):
+	try:
+		fileObject = open("userData.txt", "wb")
+		keyring.set_password("Pysms", login[0], login[1])
+		pickle.dump(login[0], fileObject, protocol=pickle.HIGHEST_PROTOCOL)
+		fileObject.close()
+	except Exception as e:
+		print("Failed to save login\n")
+		print(sys.exec_info()[0])
+
+##Purge login details if user no longer wants it saved
+def purgeLogin(number):
+	try:
+		keyring.delete_password("Pysms", number)
+		os.remove("userData.txt")
+	except Exception as e:
+		print("Error, unable to purge login details")
+		print(sys.exec_info()[0])
+
+##Check if login details are already present
+def checkLogin():
+	try:
+		fileObject = open("userData.txt", 'rb')
+		loginDetails = pickle.load(fileObject)
+		if loginDetails:
+			return [loginDetails, keyring.get_password("Pysms", loginDetails)]
+		return loginDetails
+	except:
+		print("Error checking for login details")
+
+##Process Login Details
+def processLogin(phoneNumber, password, saveDetails, retrieved, frame):
+	loginDetails = [phoneNumber, password]
+	if three.login(loginDetails):
+		if saveDetails:
+			saveLogin(loginDetails)
+		elif retrieved:
+			purgeLogin(phoneNumber)
+		destroyGrid(frame)
+		sendMessage(frame)
+	else:
+		messagebox.showerror("Unable to login", "Please check your username/password combination and try again")
+
+##Generate a login window
 def generateLogin(frame):
-	frame.wm_title("PySms")
+	frame.wm_title("Pysms")
 
-	#####Save Login Details#######
-	def saveLogin(login):
-		try:
-			fileObject = open("userData.txt", 'wb')
-			keyring.set_password("Pysms", login[0], login[1])
-			cPickle.dump(login[0], fileObject, protocol=cPickle.HIGHEST_PROTOCOL)
-			fileObject.close()
-		except:
-			log.write("Failed To Save Login \n")
-
-	#####Remove Login Details#####
-	def purgeLogin(number):
-		try:
-			keyring.delete_password("Pysms", number)
-			os.remove("userData.txt")
-		except:
-			log.write("Erro, unable to purge login data")
-
-	######Check If Login Details already present#####
-	def checkLogin():
-		try:
-			fileObject = open("userData.txt", 'r')
-			loginDetails = cPickle.load(fileObject)
-			if loginDetails:
-				return [loginDetails, keyring.get_password("Pysms", loginDetails)]
-			return loginDetails
-		except:
-			log.write("No Login Details found on System")
-			
-	######Process Login Details#####
-	def processLogin(phoneNumber, Password, saveDetails, retrieved):
-		loginDetails = [phoneNumber, Password]
-		if three.Login(loginDetails):
-			if saveDetails == 1:
-				saveLogin(loginDetails)
-			elif ((saveDetails == 0) and (retrieved == True)):
-				purgeLogin(phoneNumber)
-			destroyGrid(frame)
-			sendMessage(frame)
-		else:
-			tkMessageBox.showinfo('Login Failed', 'Please check your phone number and password and try again')
-
-	PNLabel = Label(text="Phone number")
-	PNLabel.grid(row=0, column=0, sticky="W")
-
+	##Retrieve stored info if available
 	loginNumber = loginPassword = ""
-	saveDetails = IntVar()
+	saveDetails = BooleanVar()
 	storedDetails = checkLogin()
 	retrieved = False
 
 	if storedDetails:
 		loginNumber = storedDetails[0]
 		loginPassword = storedDetails[1]
-		saveDetails.set(1)
+		saveDetails.set(True)
 		retrieved = True
 	else:
-		saveDetails.set(0)
+		saveDetails.set(False)
 
+	##Set tkinter gui
+	phoneLabel = Label(text="Phone Number")
+	phoneLabel.grid(row=0, column=0, sticky="W")
 
-	PhoneNumber = Entry(frame)
-	PhoneNumber.insert(0, loginNumber)
-	PhoneNumber.grid(row=0, column=1, padx=5, pady=5)
+	phoneEntry = Entry(frame)
+	phoneEntry.insert(END, loginNumber)
+	phoneEntry.grid(row=0, column=1, padx=5, pady=5)
 
-	PassLabel = Label(text="Password")
-	PassLabel.grid(row=1, column=0, sticky="W")
+	passLabel = Label(text="Password")
+	passLabel.grid(row=1, column=0, sticky="W")
 
-	Password = Entry(show="*")
-	Password.insert(END, loginPassword)
-	Password.grid(row=1, column=1, padx=5, pady=5)
+	passEntry = Entry(frame, show="*")
+	passEntry.insert(END, loginPassword)
+	passEntry.grid(row=1, column=1, padx=5, pady=5)
 
-
-	checkSave = Checkbutton(text="Save My Login Details", variable=saveDetails, \
-							onvalue=1, offvalue=0)
+	checkSave = Checkbutton(text="Save my login details", variable=saveDetails, onvalue=True, offvalue=False)
 	checkSave.grid(row=2, column=0, columnspan=2, pady=5)
 
-
-	B = Button(text="Login", command= lambda: processLogin(PhoneNumber.get(), Password.get(), saveDetails.get(), retrieved))
-	B.grid(row=3, column=0, columnspan=2, pady=5)
-
+	loginButton = Button(text="login", command=lambda: processLogin(phoneEntry.get(), passEntry.get(), saveDetails.get(), retrieved, frame))
+	loginButton.grid(row=3, column=0, columnspan=2, pady=5)
 
 
-#####Generate Send Message Frame
+###Methods below this line are used for sending messages #####
+
+
+##Generate the send message window
 def sendMessage(frame):
-	frame.wm_title("Send Message")
-
-	numbers = []
-	remainingTexts = -1
-	try:
-		remainingTexts = three.retrieveRemaining()
-	except:
-		log.write("Failed to Retrieve Webtext Amount")
-
-	####Process Sending Messages 
+	##Process message before sending
 	def processSend(msg, recipient):
 		msgToSend = msg.strip()
-		messageSize = (len(msgToSend) / 160)+1
+		messageSize = (len(msgToSend)/160)+1
 		totalMessages = len(numbers) * messageSize
-		if int(remainingTexts) > totalMessages:
+		if(int(remainingTexts) > totalMessages):
 			numbers.append(recipient)
-			three.Send(msgToSend, numbers)
-			tkMessageBox.showinfo('Success!', 'Your message was successfully sent!')
-			destroyGrid(frame)
-			sendMessage(frame)
+			if three.send(msgToSend, numbers):
+				messagebox.showinfo("Success", "Message was successfully sent")
+				destroyGrid(frame)
+				sendMessage(frame)
+			else:
+				messagebox.showinfo("Error", "An error occurred while sending your message. Please try again later")
 		else:
-			tkMessageBox.showinfo('Error', 'You do not have sufficient webtexts remaining to send this message')
+			messagebox.showerror("Error", "You do not have enough remaining web texts to send "+str(totalMessages)+" messages")
 
-
-	#event definition
+	##Update message length
 	def update(event):
 		if event.keycode == 8:
-			message = msg.get("1.0", 'end-1c')
-			var.set("Character Count: "+str(len(msg.get("1.0", 'end-2c'))))
-		else:	
-			var.set("Character Count: "+str(len(msg.get("1.0", 'end'))))
+			message = msg.get("1.0", "end-1c")
+			charCounter.set("Character Count: "+str(len(msg.get("1.0", "end-2c"))))
+		else:
+			charCounter.set("Character Count: "+str(len(msg.get("1.0", "end"))))
 
 	def browseVcard(browseButton):
-		from tkFileDialog import askopenfilename
-		vcard = askopenfilename(filetypes=[("Vcards", "*.vcf")])
+		from tkinter import filedialog
+		vcard = filedialog.askopenfilename(filetypes=[("Vcards", "*.vcf")])
 		display = str(vcard).split('/')
 		browseButton.set(display[-1])
 		fp = open(vcard, "r")
@@ -158,9 +136,18 @@ def sendMessage(frame):
 
 		vcards = vobject.readComponents(content)
 		for contact in vcards:
-			contact.prettyPrint()
 			numbers.append(contact.tel.value)
 
+	frame.wm_title("Send Message")
+
+	numbers = []
+	remainingTexts = -1
+	try:
+		remainingTexts = three.retrieveRemaining()
+	except:
+		print("Failed to retrieve remaining webtexts")
+
+	###Set tkinter send message gui
 	msgLabel = Label(text="Message")
 	msgLabel.grid(row=0, column=0, sticky="W", padx=10)
 	msg = Text(width=40, height=4, wrap="word", yscrollcommand="true")
@@ -169,10 +156,10 @@ def sendMessage(frame):
 	remainingLabel = Label(text="Remaining Webtexts: "+str(remainingTexts))
 	remainingLabel.grid(row=1, column=0, padx= 5, sticky="E")
 
-	var = StringVar()
-	var.set("Character Count: 0")
+	charCounter = StringVar()
+	charCounter.set("Character Count: 0")
 	msg.bind("<Key>", update)
-	charcount = Label(textvariable=var)
+	charcount = Label(textvariable=charCounter)
 	charcount.grid(row=1, column=1, pady=3, padx=30, sticky="E")
 
 	browseButton = StringVar()
@@ -180,7 +167,7 @@ def sendMessage(frame):
 	BrowseLabel = Label(text="Browse for Vcard")
 	Browse = Button(textvar=browseButton, command = lambda: browseVcard(browseButton))
 	BrowseLabel.grid(row=2, column=0, padx=10)
-	Browse.grid(row=2, column=1, padx=10, sticky="W")	
+	Browse.grid(row=2, column=1, padx=10, sticky="W")
 
 	recipientLabel = Label(text="Recipient")
 	recipientLabel.grid(row=3, column=0, padx=10, sticky="W")
--- dependencies/three.py
+++ dependencies/three.py
@@ -1,62 +1,52 @@
-import urllib
-import urllib2
+import requests
 
-log = open("../log.txt", 'wb')
+globalSession = requests.Session()
 
-def retrieveRemaining():
-	request = urllib2.Request('https://webtexts.three.ie/webtext/messages/send')
-	resp = urllib2.urlopen(request)
-	dump = str(resp.read())
-	position = dump.find("Remaining texts: ")
-	rawRemaining = dump[(position+28):(position+32)]
-	remaining = (rawRemaining.split(' '))[0]
-	return remaining
-
-##### Login to Three Ireland Website#####
-def Login(details):
-
-	login_url = 'https://webtexts.three.ie/webtext/users/login'
-
+###Login to the three website
+def login(details):
+	loginUrl = "https://webtexts.three.ie/webtext/users/login"
 	payload = {
 		'data[User][telephoneNo]' : details[0],
 		'data[User][pin]' : details[1]
 	}
 
-	data = urllib.urlencode(payload)
-
-	request = urllib2.Request(login_url, data)
-	resp = urllib2.urlopen(request)
-	contents = str(resp.geturl())
-
-	if contents == 'https://webtexts.three.ie/webtext/messages/send':
+	res = globalSession.post(loginUrl, data=payload)
+	if res.url == "https://webtexts.three.ie/webtext/messages/send":
 		return True
 	else:
 		return False
-		
 
-def Send(msg, recipient):
-	message_submission = 'https://webtexts.three.ie/webtext/messages/send'
+##Retrieve users remaining text messages
+def retrieveRemaining():
+	res = globalSession.get("https://webtexts.three.ie/webtext/messages/send")
+	dump = res.text
+	position = dump.find("Remaining texts: ")
+	rawRemaining = dump[(position+28):(position+32)]
+	remaining = (rawRemaining.split(' '))[0]
+	return remaining
+
+##Send messages, assuming we're logged in
+def send(msg, recipient):
+	messageSubmission = 'https://webtexts.three.ie/webtext/messages/send'
 	recipientCount = 0
 	while recipientCount < len(recipient):
 		payload = {
-		'data[Message][message]' : msg,
-		'data[Message][recipients_individual][0]' : recipient[recipientCount]
+			'data[Message][message]' : msg,
+			'data[Message][recipients_individual][0]' : recipient[recipientCount]
 		}
+		print(str(recipientCount+1) + " - " + str(len(recipient)))
 		if recipientCount+1 < len(recipient):
 			try:
 				payload['data[Message][recipients_individual][1]'] = recipient[recipientCount+1]
 			except:
-				log.write("Unable to add contact 2")
+				print("Unable to add contact 2")
 
 			try:
 				payload['data[Message][recipients_individual][2]'] = recipient[recipientCount+2]
 			except:
-				log.write("Unable to add contact 3")
+				print("Unable to add contact 3")
 
 		recipientCount = recipientCount+2
-
-		data = urllib.urlencode(payload)	
-		request = urllib2.Request(message_submission, data)
-		resp = urllib2.urlopen(request)
+		globalSession.post(messageSubmission, data=payload)
 
 	return True
